72e5d4251807fcffd8474ce5f23abbf29f3b62e4
Joe Moses <joe@Joes-MacBook-Pro.local>
2025-01-19 18:01:18 +0000

    meta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="description" content="Teacher Roisin is a professional English as a Second Language (ESL) teacher who is passionate about helping learners from all backgrounds improve their English skills.">
+        <meta name="keywords" content="ESL, English Teacher, Teacher Roisin">
+        <meta name="author" content="Teacher Roisin">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <title>{{ config('app.name', 'Teacher Roisin') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
